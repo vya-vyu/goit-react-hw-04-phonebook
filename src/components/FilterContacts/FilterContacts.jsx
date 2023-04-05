5820e35d3ce69b17805679b249fc8f151e1123f9
vya-vyu <linda1307@gmail.com>
2023-04-05 23:29:44 +0100

    test2

--- src/components/FilterContacts/FilterContacts.jsx
+++ src/components/FilterContacts/FilterContacts.jsx
@@ -10,21 +10,19 @@ const FilterContacts = ({ onFilter }) => {
     onFilter(filter);
   }, [filter]);
 
-  {
-    return (
-      <div className={s.filterWrapper}>
-        <label htmlFor="filter">Find contacts by name</label>
-        <input
-          id="filter"
-          type="text"
-          name="name"
-          value={filter}
-          onChange={e => setFilter(e.currentTarget.value)}
-          className={s.search}
-        />
-      </div>
-    );
-  }
+  return (
+    <div className={s.filterWrapper}>
+      <label htmlFor="filter">Find contacts by name</label>
+      <input
+        id="filter"
+        type="text"
+        name="name"
+        value={filter}
+        onChange={e => setFilter(e.currentTarget.value)}
+        className={s.search}
+      />
+    </div>
+  );
 };
 
 FilterContacts.propTypes = {
