6d98079f1f65b61a6863c934f2c8c2674780ff2b
vya-vyu <linda1307@gmail.com>
2023-04-05 23:33:40 +0100

    add use

--- src/components/FilterContacts/FilterContacts.jsx
+++ src/components/FilterContacts/FilterContacts.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import s from './FilterContacts.module.css';
 import { useEffect } from 'react';
 import PropTypes from 'prop-types';
